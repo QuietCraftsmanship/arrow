QuietCraftsmanship/arrow

commit 7a1f4bde586eb74f196e9b0bee31aebf796635fd

Remove cur_offset variable

cpp/src/arrow/compute/kernels/vector_selection_filter_internal.cc
@@ -523,11 +523,9 @@ Status BinaryFilterNonNullImpl(KernelContext* ctx, const ArraySpan& values,
         (raw_offsets[position + length] - raw_offsets[position]);
     APPEND_RAW_DATA(raw_data + raw_offsets[position], run_data_bytes);
     // Append offsets
-    offset_type cur_offset = raw_offsets[position];
     for (int64_t i = 0; i < length; ++i) {
       offset_builder.UnsafeAppend(offset);
-      offset += raw_offsets[i + position + 1] - cur_offset;
-      cur_offset = raw_offsets[i + position + 1];
+      offset += raw_offsets[i + position + 1] - raw_offsets[i + position + 1];
     }
     return Status::OK();
   };

commit 7f38ea1b6eca8adbefbd3fa38e7085cbf1c2d2ad

MINOR [C++]: Remove cur_offset variable. Correctly this time. (#36116)
Fixes an issue introduced yesterday in
https://github.com/apache/arrow/pull/35750/commits/7a1f4bde586eb74f196e9b0bee31aebf796635fd

cpp/src/arrow/compute/kernels/vector_selection_filter_internal.cc
@@ -536,7 +536,7 @@ Status BinaryFilterNonNullImpl(KernelContext* ctx, const ArraySpan& values,
     // Append offsets
     for (int64_t i = 0; i < length; ++i) {
       offset_builder.UnsafeAppend(offset);
-      offset += raw_offsets[i + position + 1] - raw_offsets[i + position + 1];
+      offset += raw_offsets[i + position + 1] - raw_offsets[i + position];
     }
     return Status::OK();
   };