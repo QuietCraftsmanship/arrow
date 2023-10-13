QuietCraftsmanship/arrow

commit 10cbc79aaa09c19f23953eac3ab7f31a9989b4fa

GH-38227: [R] Fix non-unicode character errors in nightly builds (#38232)
### Rationale for this change

We have several nightly builds failing with errors building the manual as a result of unicode characters. The unicode characters aren't new, so I'm not sure why this happened now.

### What changes are included in this PR?

Install a distribution of latex that supports unicode characters (maybe)?

### Are these changes tested?

Yes

### Are there any user-facing changes?

No
* Closes: #38227

Lead-authored-by: Dewey Dunnington <dewey@voltrondata.com>
Co-authored-by: Dewey Dunnington <dewey@fishandwhistle.net>
Signed-off-by: Jacob Wujciak-Jens <jacob@wujciak.de>

r/R/csv.R
@@ -22,8 +22,8 @@
 #' `readr::read_delim()`, and `col_select` was inspired by `vroom::vroom()`.
 #'
 #' `read_csv_arrow()` and `read_tsv_arrow()` are wrappers around
-#' `read_delim_arrow()` that specify a delimiter. `read_csv2_arrow()` uses `;`⁠ for
-#' the delimiter and `,` for the decimal point.
+#' `read_delim_arrow()` that specify a delimeter. `read_csv2_arrow()` uses `;` for
+#' the delimeter and `,` for the decimal point.
 #'
 #' Note that not all `readr` options are currently implemented here. Please file
 #' an issue if you encounter one that `arrow` should support.

r/man/read_delim_arrow.Rd
@@ -174,8 +174,8 @@ Arrow C++ options have been mapped to argument names that follow those of
 }
 \details{
 \code{read_csv_arrow()} and \code{read_tsv_arrow()} are wrappers around
-\code{read_delim_arrow()} that specify a delimiter. \code{read_csv2_arrow()} uses \verb{;}⁠ for
-the delimiter and \verb{,} for the decimal point.
+\code{read_delim_arrow()} that specify a delimeter. \code{read_csv2_arrow()} uses \verb{;} for
+the delimeter and \verb{,} for the decimal point.
 
 Note that not all \code{readr} options are currently implemented here. Please file
 an issue if you encounter one that \code{arrow} should support.

commit 5b98044c9e9aeab2fbc05bdfc4fdf69866bb9d08

MINOR: [R] Fix spelling of delimiter (#38257)
### Rationale for this change

Fix typo in docs introduced in #38232

### What changes are included in this PR?

Change spelling of "delimeter" to "delimiter"

### Are these changes tested?

No
### Are there any user-facing changes?

Just docs

Authored-by: Nic Crane <thisisnic@gmail.com>
Signed-off-by: Nic Crane <thisisnic@gmail.com>

r/R/csv.R
@@ -22,8 +22,8 @@
 #' `readr::read_delim()`, and `col_select` was inspired by `vroom::vroom()`.
 #'
 #' `read_csv_arrow()` and `read_tsv_arrow()` are wrappers around
-#' `read_delim_arrow()` that specify a delimeter. `read_csv2_arrow()` uses `;` for
-#' the delimeter and `,` for the decimal point.
+#' `read_delim_arrow()` that specify a delimiter. `read_csv2_arrow()` uses `;` for
+#' the delimiter and `,` for the decimal point.
 #'
 #' Note that not all `readr` options are currently implemented here. Please file
 #' an issue if you encounter one that `arrow` should support.

r/man/read_delim_arrow.Rd
@@ -174,8 +174,8 @@ Arrow C++ options have been mapped to argument names that follow those of
 }
 \details{
 \code{read_csv_arrow()} and \code{read_tsv_arrow()} are wrappers around
-\code{read_delim_arrow()} that specify a delimeter. \code{read_csv2_arrow()} uses \verb{;} for
-the delimeter and \verb{,} for the decimal point.
+\code{read_delim_arrow()} that specify a delimiter. \code{read_csv2_arrow()} uses \verb{;} for
+the delimiter and \verb{,} for the decimal point.
 
 Note that not all \code{readr} options are currently implemented here. Please file
 an issue if you encounter one that \code{arrow} should support.