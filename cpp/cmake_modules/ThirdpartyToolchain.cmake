QuietCraftsmanship/arrow

commit ee6e3ac3f042d9e99fae56e5addb7f75253c25f9

GH-40398: [C++] Expose protobuf dependency if opentelemetry or ORC are enabled (#40399)
### Rationale for this change

When not bundling dependencies, `libarrow.a` depends on protobuf symbols if built with ORC or opentelemetry support. However this dependency was not looked up in ArrowConfig.cmake, causing client projects using linking to `arrow_static` to fail in configuration.

### What changes are included in this PR?

This change makes it so that the dependency on `protobuf` is propagated to the installed `ArrowConfig.cmake` file when necessary.
* GitHub Issue: #40398

Authored-by: Tobias Mayer <tobim@fastmail.fm>
Signed-off-by: Sutou Kouhei <kou@clear-code.com>

cpp/cmake_modules/ThirdpartyToolchain.cmake
@@ -1812,12 +1812,17 @@ if(ARROW_WITH_PROTOBUF)
   else()
     set(ARROW_PROTOBUF_REQUIRED_VERSION "2.6.1")
   endif()
-  if(ARROW_FLIGHT)
+  if(ARROW_ORC OR ARROW_WITH_OPENTELEMETRY)
+    set(ARROW_PROTOBUF_ARROW_CMAKE_PACKAGE_NAME "Arrow")
+    set(ARROW_PROTOBUF_ARROW_PC_PACKAGE_NAME "arrow")
+  elseif(ARROW_FLIGHT)
     set(ARROW_PROTOBUF_ARROW_CMAKE_PACKAGE_NAME "ArrowFlight")
     set(ARROW_PROTOBUF_ARROW_PC_PACKAGE_NAME "arrow-flight")
   else()
-    set(ARROW_PROTOBUF_ARROW_CMAKE_PACKAGE_NAME "Arrow")
-    set(ARROW_PROTOBUF_ARROW_PC_PACKAGE_NAME "arrow")
+    message(FATAL_ERROR "ARROW_WITH_PROTOBUF must be propagated in the build tooling installation."
+                        " Please extend the mappings of ARROW_PROTOBUF_ARROW_CMAKE_PACKAGE_NAME and"
+                        " ARROW_PROTOBUF_ARROW_PC_PACKAGE_NAME for newly introduced dependencies on"
+                        " protobuf.")
   endif()
   # We need to use FORCE_ANY_NEWER_VERSION here to accept Protobuf
   # newer version such as 23.4. If we don't use it, 23.4 is processed

commit 5a2fdd1c8b1e69ebc12ea1f997c79ade092a81da

GH-40751: [C++] Fix protobuf package name setting for builds with substrait (#40753)
### Rationale for this change

The problem #40751 seems to be introduced by #40399. Though I'm not entirely sure about the purpose of that, it seems to be missing an `OR ARROW_SUBSTRAIT` in the `if` branch in https://github.com/apache/arrow/commit/ee6e3ac3f042d9e99fae56e5addb7f75253c25f9#diff-5cdc95f4e1b618f2f3ef10d370ce05a1ac05d9d401aecff3ccbb3d76bd366b6aR1815

Because other than `ARROW_ORC`, `ARROW_WITH_OPENTELEMETRY` and `ARROW_FLIGHT`, `ARROW_SUBSTRAIT` also implies `ARROW_WITH_PROTOBUF`:
https://github.com/apache/arrow/blob/ee6e3ac3f042d9e99fae56e5addb7f75253c25f9/cpp/cmake_modules/ThirdpartyToolchain.cmake#L421-L423

### What changes are included in this PR?

Add the possible missing condition of `ARROW_SUBSTRAIT` for the questioning `if` branch.

### Are these changes tested?

Manually tested.

### Are there any user-facing changes?

None.

* GitHub Issue: #40751

Lead-authored-by: Ruoxi Sun <zanmato1984@gmail.com>
Co-authored-by: Rossi Sun <zanmato1984@gmail.com>
Co-authored-by: Sutou Kouhei <kou@cozmixng.org>
Signed-off-by: Sutou Kouhei <kou@clear-code.com>

cpp/cmake_modules/ThirdpartyToolchain.cmake
@@ -1812,7 +1812,9 @@ if(ARROW_WITH_PROTOBUF)
   else()
     set(ARROW_PROTOBUF_REQUIRED_VERSION "2.6.1")
   endif()
-  if(ARROW_ORC OR ARROW_WITH_OPENTELEMETRY)
+  if(ARROW_ORC
+     OR ARROW_SUBSTRAIT
+     OR ARROW_WITH_OPENTELEMETRY)
     set(ARROW_PROTOBUF_ARROW_CMAKE_PACKAGE_NAME "Arrow")
     set(ARROW_PROTOBUF_ARROW_PC_PACKAGE_NAME "arrow")
   elseif(ARROW_FLIGHT)