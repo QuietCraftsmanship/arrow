QuietCraftsmanship/arrow

commit c4d038d6e5058f37fd3f8610672e7239fcc5d271

GH-44924: [R] Remove usage of cpp11's HAS_UNWIND_PROTECT (#45261)
### Rationale for this change

The macro is no longer required on R >= 4.0 which is our minimum version.

### What changes are included in this PR?

Remove use of HAS_UNWIND_PROTECT

### Are these changes tested?

ci
### Are there any user-facing changes?

no
* GitHub Issue: #44924

Authored-by: Jacob Wujciak-Jens <jacob@wujciak.de>
Signed-off-by: Jacob Wujciak-Jens <jacob@wujciak.de>

r/src/safe-call-into-r-impl.cpp
@@ -46,11 +46,7 @@ bool SetEnableSignalStopSource(bool enabled) {
 
 // [[arrow::export]]
 bool CanRunWithCapturedR() {
-#if defined(HAS_UNWIND_PROTECT)
   return MainRThread::GetInstance().Executor() == nullptr;
-#else
-  return false;
-#endif
 }
 
 // [[arrow::export]]

r/src/safe-call-into-r.h
@@ -29,12 +29,8 @@
 #include <functional>
 #include <thread>
 
-// Unwind protection was added in R 3.5 and some calls here use it
-// and crash R in older versions (ARROW-16201). Implementation provided
-// in safe-call-into-r-impl.cpp so that we can skip some tests
-// when this feature is not provided. This also checks that there
-// is not already an event loop registered (via MainRThread::Executor()),
-// because only one of these can exist at any given time.
+// This checks that there is not already an event loop registered (via
+// MainRThread::Executor()), because only one of these can exist at any given time.
 bool CanRunWithCapturedR();
 
 // The MainRThread class keeps track of the thread on which it is safe

commit 88288d5de5a40153b4c7caf4ec987bb3a114fb90

MINOR: [R] Clean up a linting issue from #45261 (#45575)
### Rationale for this change

Cleanup a minor formatting issue introduced in #45261

### What changes are included in this PR?

Remove two new lines

### Are these changes tested?

Yes operative changes, linter should pass

### Are there any user-facing changes?

No

Authored-by: Jonathan Keane <jkeane@gmail.com>
Signed-off-by: Antoine Pitrou <antoine@python.org>

r/src/safe-call-into-r-impl.cpp
@@ -45,9 +45,7 @@ bool SetEnableSignalStopSource(bool enabled) {
 }
 
 // [[arrow::export]]
-bool CanRunWithCapturedR() {
-  return MainRThread::GetInstance().Executor() == nullptr;
-}
+bool CanRunWithCapturedR() { return MainRThread::GetInstance().Executor() == nullptr; }
 
 // [[arrow::export]]
 std::string TestSafeCallIntoR(cpp11::function r_fun_that_returns_a_string,